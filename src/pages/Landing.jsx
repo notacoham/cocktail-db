f569f9c90ed576658c0acc09fb24cecb38bb5c64
Alex Cottam <alexcottam12@gmail.com>
2024-08-27 17:16:02 -0600

    create initial loader

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,22 @@
+import axios from 'axios';
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const cocktailSearchUrl =
+  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
+export const loader = async () => {
+  const searchTerm = 'margarita';
+  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+  console.log(response);
+
+  return 'something';
+};
 
 const Landing = () => {
+  const data = useLoaderData();
+  console.log(data);
+
   return <h1>Landing Page</h1>;
 };
 
