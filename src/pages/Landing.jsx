2a6431111de1c54d8e349cc1fcfa607dda6dab91
Alex Cottam <alexcottam12@gmail.com>
2024-08-28 17:55:45 -0600

    create CocktailCard and CocktailList components

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import CocktailList from '../components/CocktailList';
 
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
@@ -14,9 +15,12 @@ export const loader = async () => {
 
 const Landing = () => {
   const { drinks, searchTerm } = useLoaderData();
-  console.log(drinks);
 
-  return <h1>Landing Page</h1>;
+  return (
+    <>
+      <CocktailList drinks={drinks} />
+    </>
+  );
 };
 
 export default Landing;
