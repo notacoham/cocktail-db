ad755411f9c1707922ae45969aa13dea0cc0dc22
Alex Cottam <alexcottam12@gmail.com>
2024-08-28 17:43:18 -0600

    add single page error handling

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,16 +6,15 @@ const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 export const loader = async () => {
-  const searchTerm = 'margarita';
+  const searchTerm = '';
   const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
-  console.log(response);
 
-  return 'something';
+  return { drinks: response.data.drinks, searchTerm };
 };
 
 const Landing = () => {
-  const data = useLoaderData();
-  console.log(data);
+  const { drinks, searchTerm } = useLoaderData();
+  console.log(drinks);
 
   return <h1>Landing Page</h1>;
 };
