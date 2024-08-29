82ac41174408c425151b003fbaa1ed9c58edfde8
Alex Cottam <alexcottam12@gmail.com>
2024-08-29 12:31:00 -0600

    add global loader and single drink page function

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useOutletContext } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/CocktailCard';
 
 const CocktailCard = ({ img, name, id, info, glass }) => {
+  //   const data = useOutletContext();
+  //   console.log(data);
+
   return (
     <Wrapper>
       <div className="img-container">
