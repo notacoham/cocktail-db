0c0f14c95dc72458c778a117e4e65f5e24ebf0d0
Alex Cottam <alexcottam12@gmail.com>
2024-09-05 13:12:34 -0600

    add error handling to Newsletter

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -8,12 +8,17 @@ const newsletterUrl = 'https://www.course-api.com/cocktails-newsletter';
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  const response = await axios.post(newsletterUrl, data);
-  console.log(response);
 
-  toast.success(response.data.msg);
+  try {
+    const response = await axios.post(newsletterUrl, data);
+    toast.success(response.data.msg);
+    return redirect('/');
+  } catch (error) {
+    console.log(error);
+    toast.error(error?.response?.data?.msg);
 
-  return redirect('/');
+    return error;
+  }
 };
 
 const Newsletter = () => {
