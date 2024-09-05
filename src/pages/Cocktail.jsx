0e4c608f8a7fa2c4bb977a65398d38d59e822a60
Alex Cottam <alexcottam12@gmail.com>
2024-09-05 13:59:14 -0600

    add react query functionality

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -2,19 +2,34 @@ import axios from 'axios';
 import React from 'react';
 import { Link, useLoaderData, Navigate } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/CocktailPage';
+import { useQuery } from '@tanstack/react-query';
+
+const singleCocktailQuery = (id) => {
+  return {
+    queryKey: ['cocktail', id],
+    queryFn: async () => {
+      const { data } = await axios.get(`${singleCocktailUrl}${id}`);
+      return data;
+    },
+  };
+};
 
 const singleCocktailUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
-export const loader = async ({ params }) => {
-  const { id } = params;
-  const { data } = await axios.get(`${singleCocktailUrl}${id}`);
+export const loader =
+  (queryClient) =>
+  async ({ params }) => {
+    const { id } = params;
+    await queryClient.ensureQueryData(singleCocktailQuery(id));
 
-  return { id, data };
-};
+    return { id };
+  };
 
 const Cocktail = () => {
-  const { id, data } = useLoaderData();
+  const { id } = useLoaderData();
+
+  const { data } = useQuery(singleCocktailQuery(id));
 
   if (!data) return <Navigate to="/"></Navigate>;
 
